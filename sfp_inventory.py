9efc692b0840de0328467b6bb623e080717febc4
Nikola Rakonjac <kraljevo995@gmail.com>
2025-09-21 01:35:39 +0200

    cosmetic changes

--- sfp_inventory.py
+++ sfp_inventory.py
@@ -1,27 +1,28 @@
 from netmiko import ConnectHandler
 import json
 
-def cisco_cli (parametri,komande):
-    net_connect = ConnectHandler(**parametri)
+def cisco_cli (parms,commands):
+    net_connect = ConnectHandler(**parms)
     net_connect.enable
-    for cmd in komande:
+    for cmd in commands:
         output=net_connect.send_command(cmd, use_textfsm=True)
         output_1=json.dumps(output,indent=10)
     net_connect.disconnect
     print(output_1)
     sfp_module = [n for n in output if n["pid"].startswith("M125")]
-    print(f"Uredjaj {parametri['host']}")
+    print(f"Device {parms['host']}")
     for n in sfp_module:
         print(n["pid"], n["sn"], "\n")
 
 
-komande = ["terminal length 0", "show inventory"]
+commands = ["terminal length 0", "show inventory"]
 for n in range (x,y):
-    parametri = {
+    parms = {
         "device_type":"cisco_ios",
         "username":"",
         "password":"",
         "host":"10.10.10." + str(n),
         "secret":""
     }
-    cisco_cli(parametri,komande)
+    cisco_cli(parms,commands)
+
